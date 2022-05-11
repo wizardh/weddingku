6075a6f1a4c6fcc5f4159aafeb8b3982805cf2b4
Adhi W <adhi.wiradh@gmail.com>
2022-05-11 17:34:52 +0700

    ganti spacing

--- app/Views/invitation.php
+++ app/Views/invitation.php
@@ -7,11 +7,11 @@
     $bulan = month_to_bulan($month);
 ?>
 
-<div class="pt-auto pb-4 my-auto text-center zoom-wrapper" style="padding-top: 80px;" id="acara">
+<div class="pt-auto pb-auto my-auto text-center zoom-wrapper" style="padding-top: 200px; padding-bottom: 170px;" id="acara">
     <div class="mt-4 zoom" style="font-family: 'Playfair Display', serif;">
-        <h4 class="lead">The wedding of</h4>
-        <h1 class="display-4 fw-bold"><i><?= $setting->bride_nickname; ?> & <?= $setting->groom_nickname; ?></i></h1>
-        <h4 class="lead"><?= date("F jS, Y", strtotime($setting->wedding_date));?></h4>
+        <h1 >The wedding of</h1>
+        <h1 class="display-1"><i><?= $setting->bride_nickname; ?> & <?= $setting->groom_nickname; ?></i></h1>
+        <h3 ><?= date("F jS, Y", strtotime($setting->wedding_date));?></h3>
     </div>
 
     <div class="col-lg-6 mx-auto px-4 pt-4 mt-4 mb-4">
@@ -28,9 +28,9 @@
 
 <!-- BRIDE & GROOM -->
 <div class="bg-secondary text-white px-4 py-4 text-center" style="font-family: 'Playfair Display', serif;">
-    <div class="py-4">
+    <div class="py-4 mb-4">
       <div class="row">      
-        <div class="mb-4">
+        <div class="py-4">
             <h3>بِسْمِ ٱللَّٰهِ ٱلرَّحْمَٰنِ ٱلرَّحِيمِ</h3>
             <p><i>Assalamu'alaikum Warahmatullahi Wabarakatuh</i>
             <br>Dengan memohon rahmat dan ridho Allah SWT, kami bermaksud menyelenggarakan resepsi pernikahan kami:</p>
@@ -57,10 +57,10 @@
 
 <!-- COUNTDOWN -->
 <div class="container px-4 py-4 text-center" id="counter-cards">
-    <h1 class="pb-2" style="font-family: 'Playfair Display', serif;">Mark the Date!</h1>
+    <h1 class="py-4" style="font-family: 'Playfair Display', serif;">Mark the Date!</h1>
     <h4 class="lead"><?= "{$hari}, {$tanggal} {$bulan} " . date("Y", strtotime($setting->wedding_date));?></h4>
 
-    <div class="row row-cols-4 row-cols-lg-4 align-items-stretch g-4 py-4 justify-content-center" id="countdown">
+    <div class="row row-cols-4 row-cols-lg-4 align-items-stretch g-4 py-4 mb-4 justify-content-center" id="countdown">
         <div class="col-lg-3">
             <div class="card bg-secondary text-white">
                 <div class="card-body display-6 px-0" id="days">                    
@@ -97,7 +97,7 @@
 <!-- AKAD DAN LOKASI -->
 <div class="bg-secondary text-white px-4 py-4 text-center" id="lokasi">
 
-    <div class="row justify-content-center">
+    <div class="row justify-content-center pt-4">
         <div class="col-md-3"></div>
         <div class="card col-md-3 mx-auto" style="background-color: rgba(245, 245, 245, 0.2);">
             <div class="card-body">
@@ -140,7 +140,7 @@
       <br>
       <p>Sebagai bentuk pencegahan terhadap persebaran wabah COVID 19, mohon patuhi selalu segala protokol kesehatan yang ada di tempat acara.</p>
     </div>
-    <div class="col-md-12">
+    <div class="col-md-12 py-4 mb-4">
         <img class="rounded img-fluid col-sm-3" style="width:200px;max-width:40%" src="<?= base_url('assets/images/covid/1.png'); ?>" aria-label="covid1" preserveAspectRatio="xMidYMid slice" focusable="true">
         <img class="rounded img-fluid col-sm-3" style="width:200px;max-width:40%" src="<?= base_url('assets/images/covid/2.png'); ?>" aria-label="covid1" preserveAspectRatio="xMidYMid slice" focusable="true">
         <img class="rounded img-fluid col-sm-3" style="width:200px;max-width:40%" src="<?= base_url('assets/images/covid/3.png'); ?>" aria-label="covid1" preserveAspectRatio="xMidYMid slice" focusable="true">
@@ -152,13 +152,13 @@
 
 <!-- KONFIRMASI -->
 <div class="container px-4 py-4 text-center" id="buku-tamu">
-    <h1 class="pb-4" style="font-family: 'Playfair Display', serif;">Konfirmasi Kehadiran & Kirim Pesan</h1>
-    <div class="col-md-5 mx-auto justify-content-sm-center">
+    <h1 class="py-4" style="font-family: 'Playfair Display', serif;">Konfirmasi Kehadiran & Kirim Pesan</h1>
+    <div class="col-md-5 pt-4 mx-auto justify-content-sm-center">
         <div class="card">
             <div class="card-body">
             <div class="form-floating mb-3">
                 <select class="form-select" id="is_attending" name="is_attending" aria-label="kehadiran">
-                    <option value="">- - -</option>
+                    <option value="" disabled>- - -</option>
                     <option value="1" <?= ($guest->is_attending ? 'selected':'');?>>Hadir</option>
                     <option value="0" <?= ($guest->is_attending ? '':'selected');?>>Tidak Hadir</option>
                 </select>                
@@ -169,7 +169,7 @@
         </div>
     </div>
 
-    <div class="col-md-5 mx-auto justify-content-sm-center">
+    <div class="col-md-5 pb-4 mb-4 mx-auto justify-content-sm-center">
         <div class="card">
             <div class="card-body">
               <div class="mb-3">                  
@@ -189,8 +189,8 @@
 
 <!-- BUKU TAMU -->
 <div class="bg-secondary text-white px-4 py-4 text-center">
-    <h1 class="pb-4" style="font-family: 'Playfair Display', serif;">Buku Tamu</h1>
-    <div class="col-md-6 mx-auto justify-content-sm-center">
+    <h1 class="py-4" style="font-family: 'Playfair Display', serif;">Buku Tamu</h1>
+    <div class="col-md-6 py-4 mb-4 mx-auto justify-content-sm-center">
         <div class="card" style="height: 360px;">
             <div class="card-body" id="comment-card" style="overflow-y: auto; max-height: 360px;">
             <?php
@@ -233,69 +233,69 @@
 
 <!-- WISHLIST -->
 <div class="container px-4 py-4 text-center" id="wishlist">
-    <h1 class="pb-4" style="font-family: 'Playfair Display', serif;">Wishlist</h1>
+    <h1 class="py-4" style="font-family: 'Playfair Display', serif;">Wishlist</h1>
     <h4 class="lead">Kami sangat bahagia dan berterima kasih untuk menerima hadiah :)</h4>
-    <p>
+    <p class="py-4">
     <button class="btn btn-light" type="button" data-bs-toggle="collapse" data-bs-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
         Lihat Wishlist
     </button>
     </p>
-    <div class="collapse col-md-6 mx-auto justify-content-sm-center" id="collapseExample">
-            <div class="card">
-                <div class="card-body" style="overflow-x: auto; ">
-                    <div class="row">
-                        <div class="col-md-6 mx-auto">
-                            <a class="image-link" data-bs-toggle="modal" data-src="<?= base_url('assets/images/QRku_Niken.JPG'); ?>" title="QR BCA Niken">
-                            <img class="rounded img-fluid" src="<?= base_url('assets/images/QRku_Niken.JPG'); ?>" aria-label="Wishlist" focusable="true">
-                            </a>
-                        </div>
-                        <div class="col-md-6 mx-auto">
-                            <a class="image-link" data-bs-toggle="modal" data-src="<?= base_url('assets/images/QRku_Niken.JPG'); ?>" title="QR BCA Niken">
-                            <img class="rounded img-fluid" src="<?= base_url('assets/images/QRku_Niken.JPG'); ?>" aria-label="Wishlist" focusable="true">
-                            </a>
-                        </div>
+    <div class="collapse py-4 col-md-6 mx-auto justify-content-sm-center" id="collapseExample">
+        <div class="card">
+            <div class="card-body" style="overflow-x: auto; ">
+                <div class="row">
+                    <div class="col-md-6 mx-auto">
+                        <a class="image-link" data-bs-toggle="modal" data-src="<?= base_url('assets/images/QRku_Niken.JPG'); ?>" title="QR BCA Niken">
+                        <img class="rounded img-fluid" src="<?= base_url('assets/images/QRku_Niken.JPG'); ?>" aria-label="Wishlist" focusable="true">
+                        </a>
                     </div>
-                    <div class="row">
-                        <h4 class="pt-4">Wishlist Produk</h4>
-                    <table class="table table-striped">
-                        <thead>
-                            <tr>
-                            <th scope="col">Item</th>
-                            <th scope="col">Estimasi Harga</th>
-                            <th scope="col">Referensi</th>
-                            <th scope="col">Status</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                            <td>Rumah<br><img src="<?= base_url('assets/images/internet_404_page_not_found.png'); ?>" class="img-thumbnail image-link rounded float-end" data-src="<?= base_url('assets/images/internet_404_page_not_found.png'); ?>"></td>
-                            <td><?= number_format(10000000000, 0, ',','.');?></td>
-                            <td>Link Toped</td>
-                            <td><button type="button" class="btn btn-success" style="width: 6em">Open</button</td>
-                            </tr>
-                            <tr>
-                            <td>Mobil<br><img src="<?= base_url('assets/images/internet_404_page_not_found.png'); ?>" class="img-thumbnail image-link rounded float-end" data-src="<?= base_url('assets/images/internet_404_page_not_found.png'); ?>"></td>
-                            <td><?= number_format(1000000000, 0, ',','.');?></td>
-                            <td>Link Toped</td>
-                            <td><button type="button" class="btn btn-warning" style="width: 6em" disabled>Booked!</button</td>
-                            </tr>
-                            <tr>
-                        </tbody>
-                    </table>
-
+                    <div class="col-md-6 mx-auto">
+                        <a class="image-link" data-bs-toggle="modal" data-src="<?= base_url('assets/images/QRku_Niken.JPG'); ?>" title="QR BCA Niken">
+                        <img class="rounded img-fluid" src="<?= base_url('assets/images/QRku_Niken.JPG'); ?>" aria-label="Wishlist" focusable="true">
+                        </a>
                     </div>
+                </div>
+                <div class="row">
+                    <h4 class="pt-4">Wishlist Produk</h4>
+                <table class="table table-striped">
+                    <thead>
+                        <tr>
+                        <th scope="col">Item</th>
+                        <th scope="col">Estimasi Harga</th>
+                        <th scope="col">Referensi</th>
+                        <th scope="col">Status</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                        <td>Rumah<br><img src="<?= base_url('assets/images/internet_404_page_not_found.png'); ?>" class="img-thumbnail image-link rounded float-end" data-src="<?= base_url('assets/images/internet_404_page_not_found.png'); ?>"></td>
+                        <td><?= number_format(10000000000, 0, ',','.');?></td>
+                        <td>Link Toped</td>
+                        <td><button type="button" class="btn btn-success" style="width: 6em">Open</button</td>
+                        </tr>
+                        <tr>
+                        <td>Mobil<br><img src="<?= base_url('assets/images/internet_404_page_not_found.png'); ?>" class="img-thumbnail image-link rounded float-end" data-src="<?= base_url('assets/images/internet_404_page_not_found.png'); ?>"></td>
+                        <td><?= number_format(1000000000, 0, ',','.');?></td>
+                        <td>Link Toped</td>
+                        <td><button type="button" class="btn btn-warning" style="width: 6em" disabled>Booked!</button</td>
+                        </tr>
+                        <tr>
+                    </tbody>
+                </table>
 
                 </div>
-            </div>        
+
+            </div>
+        </div>        
     </div>
 
 </div>
 <!-- /WISHLIST -->
-<!-- BRIDE & GROOM -->
-<div class="bg-secondary text-white px-4 py-4 text-center" style="font-family: 'Playfair Display', serif;">
-    <h1 class="pb-4" style="font-family: 'Playfair Display', serif;">Terima Kasih!</h1>
+<!-- TERIMA KASIH -->
+<div class="bg-secondary text-white px-auto py-4 text-center" style="font-family: 'Playfair Display', serif;">
+    <h1 class="py-4" style="font-family: 'Playfair Display', serif;">Terima Kasih!</h1>
     <div class="py-4">
-      <div class="col-lg-10 mx-auto">
+      <div class="col-lg-8 mx-auto">
         <figure>
             <p>"Dan di antara tanda-tanda kekuasaan-Nya ialah diciptakan-Nya untukmu pasangan hidup dari jenismu sendiri supaya kamu mendapat ketenangan hati, dan dijadikan-Nya rasa kasih sayang di antara kamu. Sesungguhnya yang demikian menjadi tanda-tanda kebesaran-Nya bagi orang-orang yang berpikir."</p>
             <figcaption class="blockquote-footer text-white">
@@ -305,6 +305,7 @@
       </div>
     </div>
 </div>
+<!-- /TERIMA KASIH -->
 
 <!-- Modal -->
 <div class="modal fade" id="popupModal" tabindex="-1" aria-labelledby="popupModalLabel" aria-hidden="true">
@@ -439,7 +440,7 @@ $(document).ready( function () {
                     dataType: 'json',
                     data: { guest_id:guest_id, message:message },             
                     success: function(data) {
-                        Swal.fire('Pesan telah dikirim!', '', 'success');
+                        Swal.fire('Terima kasih atas pesannya!', '', 'success');
 
                         message_html = 
                             `<figure><blockquote class="blockquote text-black"><p>${message}</p></blockquote><figcaption class="blockquote-footer"><?= $guest->name; ?> <cite title="Source Title">Private (Unpublished)</cite></figcaption></figure>`;
