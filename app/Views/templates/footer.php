3e4975694860926d5ed20bab5eafe74c0c84500b
wizardh <adhi.wiradh@gmail.com>
2022-08-02 23:01:09 +0700

    footer ketinggalan

--- app/Views/templates/footer.php
+++ app/Views/templates/footer.php
@@ -1,5 +1,5 @@
 
-<div style="background-color:#FEADAD">
+<div style="background-color:<?= $setting->bg_color; ?>">
   <footer class="container d-flex justify-content-between align-items-center py-3 text-light" style="font-family: 'Playfair Display', serif;">
     <p class="col-md-4 mb-0">&copy; <?= date('Y'); ?> Niken & Aya</p>
 
